c0f882ced629adadc13a13ee173a025070ff007a
David Folk <davidwfolk@gmail.com>
2020-06-01 18:16:49 -0600

    highlighting disappearing

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.addEventListener('keydown', function(e) {
-  const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`)
-  const key = this.document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
 if (!audio) return;
 // @ts-ignore
 audio.currentTime = 0;
@@ -10,3 +10,10 @@ key.classList.add('playing')
   
 })
 
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
